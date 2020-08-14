6367df9e23e029861bbcb8703b91e30ecff2af1d
fdonatto <fdonatto@gmail.com>
2020-08-14 20:48:36 -0300

    Criando entity Post com User aninhado

--- src/main/java/com/fdsoftware/config/Instantiation.java
+++ src/main/java/com/fdsoftware/config/Instantiation.java
@@ -1,12 +1,16 @@
 package com.fdsoftware.config;
 
+import java.text.SimpleDateFormat;
 import java.util.Arrays;
+import java.util.TimeZone;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Configuration;
 
+import com.fdsoftware.domain.Post;
 import com.fdsoftware.domain.User;
+import com.fdsoftware.repositories.PostRepository;
 import com.fdsoftware.repositories.UserRepository;
 
 @Configuration
@@ -14,6 +18,9 @@ public class Instantiation implements CommandLineRunner {
 	
 	@Autowired
 	private UserRepository userRepository;
+	
+	@Autowired
+	private PostRepository postRepository;
 
 	@Override
 	public void run(String... args) throws Exception {
@@ -23,6 +30,13 @@ public class Instantiation implements CommandLineRunner {
 		User alex = new User(null, "Alex Green", "alex@gmail.com");
 		User bob = new User(null, "Bob Grey", "bob@gmail.com");
 		userRepository.saveAll(Arrays.asList(maria, alex, bob));
+		
+		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+		sdf.setTimeZone(TimeZone.getTimeZone("GMT"));		
+		Post post = new Post(null, sdf.parse("21/03/2018"), "Partiu viagem", "Vou viajar para São Paulo. Abraços!", maria);
+		Post post2 = new Post(null, sdf.parse("23/03/2018"), "bom dia", "Acordei feliz hoje!", maria);
+		
+		postRepository.saveAll(Arrays.asList(post, post2));
 		
 	}
 	
