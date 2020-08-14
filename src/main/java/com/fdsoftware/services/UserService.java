a9d574e21ad3aab2421c8c6697fb1bfba3233f0b
fdonatto <fdonatto@gmail.com>
2020-08-14 16:51:00 -0300

    Inserção de usuário com POST

--- src/main/java/com/fdsoftware/services/UserService.java
+++ src/main/java/com/fdsoftware/services/UserService.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.fdsoftware.domain.User;
+import com.fdsoftware.dto.UserDTO;
 import com.fdsoftware.repositories.UserRepository;
 import com.fdsoftware.service.exception.ObjectNotFoundException;
 
@@ -23,6 +24,14 @@ public class UserService {
 	public User findById(String id) {
 		Optional<User> user = repo.findById(id);
 		return user.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+	}
+	
+	public User insert(User user) {
+		return repo.insert(user);
+	}
+	
+	public User fromDTO(UserDTO userDto) {
+		return new User(userDto.getId(), userDto.getName(), userDto.getEmail());
 	}
 
 }
