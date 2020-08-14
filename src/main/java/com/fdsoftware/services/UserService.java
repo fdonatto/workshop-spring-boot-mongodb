3a797e69a9fa6facfc0306133f4b75eef15c599f
fdonatto <fdonatto@gmail.com>
2020-08-14 16:26:40 -0300

    Obtendo um usuário por id

--- src/main/java/com/fdsoftware/services/UserService.java
+++ src/main/java/com/fdsoftware/services/UserService.java
@@ -1,21 +1,28 @@
 package com.fdsoftware.services;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.fdsoftware.domain.User;
 import com.fdsoftware.repositories.UserRepository;
+import com.fdsoftware.service.exception.ObjectNotFoundException;
 
 @Service
 public class UserService {
-	
+
 	@Autowired
 	private UserRepository repo;
-	
+
 	public List<User> findAll() {
-		return repo.findAll();		
+		return repo.findAll();
+	}
+
+	public User findById(String id) {
+		Optional<User> user = repo.findById(id);
+		return user.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
 	}
 
 }
