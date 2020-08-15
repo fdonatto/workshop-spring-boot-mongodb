ec0eb38b0d79380e9829ae3dfc71c11e13f91a59
fdonatto <fdonatto@gmail.com>
2020-08-14 21:21:12 -0300

    Endpoint para retornar os posts de um usuário

--- src/main/java/com/fdsoftware/resources/UserResorce.java
+++ src/main/java/com/fdsoftware/resources/UserResorce.java
@@ -14,6 +14,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import com.fdsoftware.domain.Post;
 import com.fdsoftware.domain.User;
 import com.fdsoftware.dto.UserDTO;
 import com.fdsoftware.services.UserService;
@@ -59,6 +60,12 @@ public class UserResorce {
 		user.setId(id);
 		user = service.update(user);
 		return ResponseEntity.noContent().build();
+	}
+	
+	@RequestMapping(value = "/{id}/posts", method = RequestMethod.GET)
+	public ResponseEntity<List<Post>> findPosts(@PathVariable String id) {
+		User user = service.findById(id);
+		return ResponseEntity.ok().body(user.getPosts());
 	}
 
 }
