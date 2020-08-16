4e463ec96b068a1688e720104b9976ac9f7eeb4d
fdonatto <fdonatto@gmail.com>
2020-08-15 21:21:58 -0300

    Consulta simples com query methods

--- src/main/java/com/fdsoftware/resources/PostResorce.java
+++ src/main/java/com/fdsoftware/resources/PostResorce.java
@@ -1,13 +1,17 @@
 package com.fdsoftware.resources;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.fdsoftware.domain.Post;
+import com.fdsoftware.resources.util.URL;
 import com.fdsoftware.services.PostService;
 
 @RestController
@@ -21,5 +25,13 @@ public class PostResorce {
 	public ResponseEntity<Post> findById(@PathVariable String id) {
 		Post post = service.findById(id);
 		return ResponseEntity.ok().body(post);
+	}
+	
+	//url para teste no postman: http://localhost:8080/posts/titlesearch?title=bom%20dia
+	@RequestMapping(value = "/titlesearch", method = RequestMethod.GET)
+	public ResponseEntity<List<Post>> findByTitle(@RequestParam(value = "title", defaultValue = "") String title) {
+		title = URL.decodeParam(title);
+		List<Post> posts = service.findByTitle(title);
+		return ResponseEntity.ok().body(posts);
 	}
 }
